bcced8188971a6192330f8e1bdf351d04af77929
Jhonn Jairo Avellaneda Avellaneda <jhavellanedaa@darkstar.org>
2019-09-13 09:33:07 -0500

    tarea hola

---
+++ 2019-09-13/hola.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+  
+{
+  std::string name;
+  std::cout << "¿Cual es tu nombre?" << "\n";
+  std::cin >> name;
+  std::cout << "!Hola," << name << "¡" "\n";
+
+  return 0;
+}
