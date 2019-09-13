7903fd28a3bcd9564f0ca9edda691be30d3daf05
Jhonn Jairo Avellaneda Avellaneda <jhavellanedaa@darkstar.org>
2019-09-13 09:44:56 -0500

    tarea1.1

---
+++ 2019-09-13/multp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void)
+{
+  double a;
+  std::cout << "Ingrese el numero que desea duplicar:" << "\n";
+  std::cin >> a;
+  std::cout << "el resultado es:"<<"\n";
+  std::cout <<2*a<<"\n";
+
+
+  return 0;
+}
